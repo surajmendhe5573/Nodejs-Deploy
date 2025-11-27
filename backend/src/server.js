25aa811ced815cbb4bf2027785c52521761b1762
surajmendhe5573 <surajmendhe470@gmail.com>
2025-11-27 19:31:23 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import './config/env.js';
+
+import { config } from 'dotenv';
+config();
+
+import app from './app.js';
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT} in ${process.env.NODE_ENV}`);
+  console.log("DB_URL:", process.env.DB_URL); 
+});
