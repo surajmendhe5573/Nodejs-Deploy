721dbed1091341c6105ae050508e95b85724e050
surajmendhe5573 <surajmendhe470@gmail.com>
2025-12-01 11:20:13 +0530

    api changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -39,6 +39,10 @@ app.get('/contact', (req, res) => {
      res.send('This is contact page !');
 })
 
+app.get('/cicd', (req, res) => {
+     res.send('This is contact page !');
+})
+
 app.use('/api/v1/user', userRoute);
 
 app.use(notFound);
